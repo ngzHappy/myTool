62a08535c0dbed1747a2fb1a6702e8556bbcde58
ngzHappy <zhaixueqiang@hotmail.com>
2015-11-29 10:12:31 +0800

    +vecotr

--- Containers/Vector.hpp
+++ Containers/Vector.hpp
@@ -10,7 +10,9 @@ namespace cct{
     template<typename T>
     class Vector : public std::shared_ptr< std::vector<T> > {
     private:
+        typedef typename std::vector<T>::size_type size_type ;
         typedef std::shared_ptr< std::vector<T> > Super;
+        const std::vector<T> * _get_const()const{return this->get();}
     public:
 
         Vector( decltype(nullptr) ) {}
@@ -26,7 +28,7 @@ namespace cct{
         Vector&operator=(Vector&&)=default;
 
         auto operator[]( size_type n )->decltype( this->get()->operator[](n)  ) {
-            return this->get()->operator[](n);
+            return this->get()->operator[ ](n);
         }
 
         auto operator[]( size_type n )const ->decltype( this->_get_const()->operator[](n)  ) {
