80391b36f36a540b94c1d5ea5c29c226a1ac4685
ngzHappy <819869472@qq.com>
2016-01-15 11:46:17 +0800

    +const tool

--- ConstTool/main.cpp
+++ ConstTool/main.cpp
@@ -5,6 +5,9 @@
 template<typename _const_T> static inline const _const_T & _as_const(const _const_T & value) {return value ;}
 template<typename _const_T> static inline auto & _no_const(_const_T & value) {return const_cast<  std::remove_const_t< std::remove_reference_t<_const_T> > &>(value); }
 
+//auto _as_const=[](const auto & value)->auto & {return static_cast<std::add_const_t< std::remove_reference_t< decltype(value) > > &>(value);  };
+//auto _no_const=[](auto & value)->auto & { return const_cast<std::remove_const_t< std::remove_reference_t< decltype(value) > > &>(value); };
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
